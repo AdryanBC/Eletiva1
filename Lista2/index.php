f42dcd15c220441480a5ce502061b538f3aeac88
AdryanBC <adryancorrea06@gmail.com>
2026-09-23 01:13:00 -0300

    Modificação no Index, inserção do index.php para melhor navegação.

---
+++ Lista2/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <title>Lista 2</title>
+</head>
+<body>
+    <h1>Exercícios da Lista 2</h1>
+    <ul>
+        <li><a href="ex1.php">Exercício 1</a></li>
+        <li><a href="ex2.php">Exercício 2</a></li>
+        <li><a href="ex3.php">Exercício 3</a></li>
+        <li><a href="ex4.php">Exercício 4</a></li>
+        <li><a href="ex5.php">Exercício 5</a></li>
+        <li><a href="ex6.php">Exercício 6</a></li>
+        <li><a href="ex7.php">Exercício 7</a></li>
+        <li><a href="ex8.php">Exercício 8</a></li>
+        <li><a href="ex9.php">Exercício 9</a></li>
+    </ul>
+</body>
+</html>
