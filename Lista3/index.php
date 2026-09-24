2bff5398710737a44050ef5ff6fc6a239d0e499f
AdryanBC <adryancorrea06@gmail.com>
2026-09-24 00:50:51 -0300

    Modificação no Index, inserção do index.php na lista 3.

---
+++ Lista3/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <title>Lista 1</title>
+</head>
+<body>
+    <h1>Exercícios da Lista 1</h1>
+    <ul>
+        <li><a href="ex1.php">Exercício 1</a></li>
+        <li><a href="ex2.php">Exercício 2</a></li>
+        <li><a href="ex3.php">Exercício 3</a></li>
+        <li><a href="ex4.php">Exercício 4</a></li>
+        <li><a href="ex5.php">Exercício 5</a></li>
+        <li><a href="ex6.php">Exercício 6</a></li>
+        <li><a href="ex7.php">Exercício 7</a></li>
+        <li><a href="ex8.php">Exercício 8</a></li>
+        <li><a href="ex9.php">Exercício 9</a></li>
+        <li><a href="ex10.php">Exercício 10</a></li>
+        <li><a href="ex11.php">Exercício 11</a></li>
+        <li><a href="ex12.php">Exercício 12</a></li>
+        <li><a href="ex13.php">Exercício 13</a></li>
+        <li><a href="ex14.php">Exercício 14</a></li>
+        <li><a href="ex15.php">Exercício 15</a></li>
+    </ul>
+</body>
+</html>
